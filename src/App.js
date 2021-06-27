a881c1e4a85b30dc9c7297fc643bb36a35a070cd
camelot1399 <camelot1399@mail.ru>
2021-06-27 09:46:09 +0300

    dev4

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ const ROUTES = {
 
 
 const App = () => {
-  const [activePanel, setActivePanel] = useState(ROUTES.matches);
+  const [activePanel, setActivePanel] = useState(ROUTES.MATCHES);
   const [fetchedUser, setUser] = useState(null);
   const [popout, setPopout] = useState(<ScreenSpinner size='large'/>);
 
