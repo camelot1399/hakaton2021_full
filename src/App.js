2cb95eedcb0e6a5bd2d941b03c6a831c8b1d4ac7
kamil-87 <k.kamil87-87@yandex.ru>
2021-06-27 09:56:56 +0500

    create panels structure

--- src/App.js
+++ src/App.js
@@ -1,59 +1,118 @@
 import React, {useState, useEffect} from 'react';
 import bridge from '@vkontakte/vk-bridge';
-import {View, ScreenSpinner, AdaptivityProvider, AppRoot} from '@vkontakte/vkui';
+import {
+  View,
+  ScreenSpinner,
+  AdaptivityProvider,
+  AppRoot,
+  Panel,
+  PanelHeader,
+  Group,
+  Tabs,
+  TabsItem, SizeType
+} from '@vkontakte/vkui';
 import '@vkontakte/vkui/dist/vkui.css';
 import {store} from "./store"
 
-import Home from './panels/Home';
+import Matches from './panels/Matches/Matches';
+import Match from './panels/Matches/Match/Match';
+import News from './panels/News/News';
+
 import {Provider} from 'react-redux';
+import {Icon20CommentOutline, Icon20ShareOutline, Icon20ViewOutline} from "@vkontakte/icons";
 
 const ROUTES = {
-  HOME: 'home',
+  MATCHES: 'matches',
+  NEWS: 'news',
+  MATCH: 'match',
 };
 
-const STORAGE_KEYS = {
-  STATUS: 'status',
-};
 
 const App = () => {
-  const [activePanel, setActivePanel] = useState(ROUTES.HOME);
+  const [activePanel, setActivePanel] = useState(ROUTES.MATCH);
   const [fetchedUser, setUser] = useState(null);
   const [popout, setPopout] = useState(<ScreenSpinner size='large'/>);
 
-  useEffect(() => {
-    bridge.subscribe(({detail: {type, data}}) => {
-      if (type === 'VKWebAppUpdateConfig') {
-        const schemeAttribute = document.createAttribute('scheme');
-        schemeAttribute.value = data.scheme ? data.scheme : 'client_light';
-        document.body.attributes.setNamedItem(schemeAttribute);
-      }
-    });
-
-    async function fetchData() {
-      const user = await bridge.send('VKWebAppGetUserInfo');
-      setUser(user);
-      setPopout(null);
-    }
-
-    fetchData();
-  }, []);
 
   const go = e => {
     setActivePanel(e.currentTarget.dataset.to);
   };
 
-
   return (
-      <Provider store={store}>
-        <AdaptivityProvider>
-          <AppRoot>
-            <View activePanel={activePanel}>
-              <Home id={ROUTES.HOME} fetchedUser={fetchedUser} go={go}/>
-              <View activePanel={activePanel} popout={popout}></View>
-            </View>
-          </AppRoot>
-        </AdaptivityProvider>
-      </Provider>
+    <Provider store={store}>
+      <AdaptivityProvider>
+        <AppRoot>
+          <View activePanel={activePanel}>
+            <Panel id="matches">
+              <Group>
+                <Tabs>
+                  <TabsItem
+                    go={ROUTES.NEWS}
+                  >
+                    Новости
+                  </TabsItem>
+                  <TabsItem
+                    go={ROUTES.MATCHES}
+                    route={ROUTES.MATCHES}
+                  >
+                    Матчи
+                  </TabsItem>
+                  <TabsItem
+                  >
+                    Матч
+                  </TabsItem>
+                </Tabs>
+
+              </Group>
+              <News />
+              <Matches />
+            </Panel>
+            <Panel id="news">
+              <Group>
+                <Tabs>
+                  <TabsItem
+
+                  >
+                    Новости
+                  </TabsItem>
+                  <TabsItem
+                  >
+                    Матчи
+                  </TabsItem>
+                  <TabsItem
+                  >
+                    Чат
+                  </TabsItem>
+                </Tabs>
+
+              </Group>
+              <News />
+            </Panel>
+            <Panel id="match">
+              <Group>
+                <Tabs>
+                  <TabsItem
+
+                  >
+                    Новости
+                  </TabsItem>
+                  <TabsItem
+                  >
+                    Матчи
+                  </TabsItem>
+                  <TabsItem
+                  >
+                    Чат
+                  </TabsItem>
+                </Tabs>
+
+              </Group>
+              <Match />
+            </Panel>
+          </View>
+        </AppRoot>
+      </AdaptivityProvider>
+    </Provider>
   );
 }
 
