284cd8693fa1917682a58d7274136b4a947bf6b8
camelot1399 <camelot1399@mail.ru>
2021-06-27 16:50:31 +0300

    dev13

--- src/panels/News/News.js
+++ src/panels/News/News.js
@@ -1,8 +1,13 @@
 import React from "react"
-import {Div, Card, CardScroll, Cell, Panel, PanelHeader, Search, Badge, ContentCard} from "@vkontakte/vkui";
+import {Div, Card, CardScroll, Cell, Panel, PanelHeader, Search, Badge, ContentCard, Group, CardGrid} from "@vkontakte/vkui";
 import { Icon56GalleryOutline } from '@vkontakte/icons';
 import { Icon28ClipOutline } from '@vkontakte/icons';
 import { Icon28LiveOutline } from '@vkontakte/icons';
+import { Icon16LikeOutline } from '@vkontakte/icons';
+import { Icon24MessageOutline } from '@vkontakte/icons';
+import { Icon16Repost } from '@vkontakte/icons';
+import { Icon16ViewOutline } from '@vkontakte/icons';
+import {CardGameScroller} from '../../components/plugins/cardGameScroller/cardGameScroller';
 
 import classname from './News.css'
 
@@ -11,53 +16,7 @@ class PanelNews extends React.Component {
 
     return (
       <>
-        <CardScroll size="s" style={{marginBottom: '10px'}}>
-          <Card>
-            <div className="game">
-              <span>cs:go</span>
-              <img
-                src="https://www.digiseller.ru/preview/488198/p1_2697660_c9964c10.jpg"
-                alt=""
-                width="50"/>
-            </div>
-          </Card>
-          <Card>
-            <div className="game">
-              <span>cs:go</span>
-              <img
-                src="https://www.digiseller.ru/preview/488198/p1_2697660_c9964c10.jpg"
-                alt=""
-                width="50"/>
-            </div>
-          </Card>
-          <Card>
-            <div className="game">
-              <span>cs:go</span>
-              <img
-                src="https://www.digiseller.ru/preview/488198/p1_2697660_c9964c10.jpg"
-                alt=""
-                width="50"/>
-            </div>
-          </Card>
-          <Card>
-            <div className="game">
-              <span>cs:go</span>
-              <img
-                src="https://www.digiseller.ru/preview/488198/p1_2697660_c9964c10.jpg"
-                alt=""
-                width="50"/>
-            </div>
-          </Card>
-          <Card>
-            <div className="game">
-              <span>cs:go</span>
-              <img
-                src="https://www.digiseller.ru/preview/488198/p1_2697660_c9964c10.jpg"
-                alt=""
-                width="50"/>
-            </div>
-          </Card>
-        </CardScroll>
+        <CardGameScroller/>
 
         <div style={{display: "flex", alignItems: 'center', marginBottom: '10px'}}>
         <Search placeholder='Поиск по новостям' />
@@ -75,28 +34,129 @@ class PanelNews extends React.Component {
           </div>
         </div>
 
-        <CardScroll size="s" >
-          <ContentCard style={{marginRight: '10px', marginBottom: '10px'}}
-            image="https://images.unsplash.com/photo-1603988492906-4fb0fb251cf8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1600&q=80"
-            text="NaVi заканчивают свой путь на квалификациях к The International"
-            maxHeight={165}
-            maxWidht={125}
-          />
+        
+        <div className="wrap">
+          <CardScroll size="s" >
+            <ContentCard style={{marginRight: '10px', marginBottom: '10px'}}
+              image="https://images.unsplash.com/photo-1603988492906-4fb0fb251cf8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1600&q=80"
+              text="NaVi заканчивают свой путь на квалификациях к The International"
+              maxHeight={165}
+              maxWidht={125}
+            />
 
-          <ContentCard style={{marginRight: '10px', marginBottom: '10px'}}
-            image="https://images.unsplash.com/photo-1603988492906-4fb0fb251cf8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1600&q=80"
-            text="NaVi заканчивают свой путь на квалификациях к The International"
-            maxHeight={165}
-            maxWidht={125}
-          />
-          <ContentCard style={{ marginBottom: '10px'}}
-            image="https://images.unsplash.com/photo-1603988492906-4fb0fb251cf8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1600&q=80"
-            text="NaVi заканчивают свой путь на квалификациях к The International"
-            maxHeight={165}
-            maxWidht={125}
-          />
-        </CardScroll>
+            <ContentCard style={{marginRight: '10px', marginBottom: '10px'}}
+              image="https://images.unsplash.com/photo-1603988492906-4fb0fb251cf8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1600&q=80"
+              text="NaVi заканчивают свой путь на квалификациях к The International"
+              maxHeight={165}
+              maxWidht={125}
+            />
+            <ContentCard style={{ marginBottom: '10px'}}
+              image="https://images.unsplash.com/photo-1603988492906-4fb0fb251cf8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1600&q=80"
+              text="NaVi заканчивают свой путь на квалификациях к The International"
+              maxHeight={165}
+              maxWidht={125}
+            />
+            <ContentCard style={{ marginBottom: '10px'}}
+              image="https://images.unsplash.com/photo-1603988492906-4fb0fb251cf8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1600&q=80"
+              text="NaVi заканчивают свой путь на квалификациях к The International"
+              maxHeight={165}
+              maxWidht={125}
+            />
+            <ContentCard style={{ marginBottom: '10px'}}
+              image="https://images.unsplash.com/photo-1603988492906-4fb0fb251cf8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1600&q=80"
+              text="NaVi заканчивают свой путь на квалификациях к The International"
+              maxHeight={165}
+              maxWidht={125}
+            />
+            <ContentCard style={{ marginBottom: '10px'}}
+              image="https://images.unsplash.com/photo-1603988492906-4fb0fb251cf8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1600&q=80"
+              text="NaVi заканчивают свой путь на квалификациях к The International"
+              maxHeight={165}
+              maxWidht={125}
+            />
+          </CardScroll>
+
+          <div className="lastNews">
+            <div className="lastNews__time">1 час назад</div>
+            <div className="lastNews__imgBlock">
+              <a href="#" className="lastNews__a">
+                <img 
+                  className="lastNews__img"
+                  src="https://images.unsplash.com/photo-1603988492906-4fb0fb251cf8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1600&q=80"></img>
+                <div className="lastNews__text">
+                  NaVi заканчивают свой путь на квалификациях к The International
+                </div>
+                <div className="lastNews__totalBoard">
+                  <div className="lastNews__leftBoard">
+                    <div className="line lastNews__like">
+                      <Icon16LikeOutline /> <span>65</span>
+                    </div>
+                    <div className="line lastNews__message">
+                      <Icon24MessageOutline />
+                      <span>65</span>
+                    </div>
+                    <div className="line lastNews__repost">
+                      <Icon16Repost />
+                      <span>4</span>
+                    </div>
+                  </div>
+                  <div className="line lastNews__views">
+                    <Icon16ViewOutline />
+                    <span>7,2k</span>
+                  </div>
+                </div>
+              </a>
+            </div>
+          </div>
+
+          <Group>
+            <CardGrid size="l">
+              <ContentCard
+                subtitle="VKUI"
+                header="ContentCard example"
+                caption="VKUI Styleguide > Blocks > ContentCard"
+              />
+              <ContentCard
+                image="https://images.unsplash.com/photo-1603988492906-4fb0fb251cf8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1600&q=80"
+                subtitle="unsplash"
+                header="NaVi заканчивают свой путь на квалификациях к The International"
+                text="NaVi заканчивают свой путь на квалификациях к The International"
+                caption="NaVi заканчивают свой путь на квалификациях к The International"
+                maxHeight={150}
+              />
+              <ContentCard
+                image="https://images.unsplash.com/photo-1603988492906-4fb0fb251cf8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1600&q=80"
+                subtitle="unsplash"
+                header="NaVi заканчивают свой путь на квалификациях к The International"
+                text="NaVi заканчивают свой путь на квалификациях к The International"
+                caption="NaVi заканчивают свой путь на квалификациях к The International"
+                maxHeight={150}
+              />
+              <ContentCard
+                image="https://images.unsplash.com/photo-1603988492906-4fb0fb251cf8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1600&q=80"
+                subtitle="unsplash"
+                header="NaVi заканчивают свой путь на квалификациях к The International"
+                text="NaVi заканчивают свой путь на квалификациях к The International"
+                caption="NaVi заканчивают свой путь на квалификациях к The International"
+                maxHeight={150}
+              />
+              <ContentCard
+                image="https://images.unsplash.com/photo-1603988492906-4fb0fb251cf8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1600&q=80"
+                subtitle="unsplash"
+                header="NaVi заканчивают свой путь на квалификациях к The International"
+                text="NaVi заканчивают свой путь на квалификациях к The International"
+                caption="NaVi заканчивают свой путь на квалификациях к The International"
+                maxHeight={150}
+              />
+            </CardGrid>
+          </Group>
+        </div>
+        
       </>
+
+      
+
+
     )
   }
 }
